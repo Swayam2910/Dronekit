8fa8860ae644c5085c570e84de4e8ed62ed4b81d
Swayam2910 <70374043+Swayam2910@users.noreply.github.com>
2021-05-24 20:30:12 +0530

    Update hello_drone.py

--- hello_drone.py
+++ hello_drone.py
@@ -1,14 +1,8 @@
-print("Start simulator (SITL)")
-import dronekit_sitl
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
-
 # Import DroneKit-Python
 from dronekit import connect, VehicleMode
 
 # Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=True)
+vehicle = connect('127.0.0.1:14550', wait_ready=True) #UDP connection
 
 # Get some vehicle attributes (state)
 print("Get some vehicle attribute values:")
